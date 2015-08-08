f55238ee34ce992dbcd0e251247b4f40653794f7
havenspool <havenspool@gmail.com>
2015-08-08 15:32:13 +0800

    20150806

--- src/main/java/com/havens/nettydemo/message/Message.java
+++ src/main/java/com/havens/nettydemo/message/Message.java
@@ -2,22 +2,22 @@ package com.havens.nettydemo.message;
 
 import org.json.JSONObject;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by havens on 15-8-7.
  */
-public class Message extends JSONObject {
+public class Message extends JSONObject implements Serializable{
     public String cmd;
     public Map data;
 
-
-    public static void main(String[] args){
-        Message msg=new Message();
-        msg.cmd="time_check";
-        msg.data=new HashMap();
-        msg.put(msg.cmd,msg.data);
+    public static void main(String[] args) {
+        Message msg = new Message();
+        msg.cmd = "time_check";
+        msg.data = new HashMap();
+        msg.put(msg.cmd, msg.data);
         System.out.println(msg.toString());
     }
 }
